89041c486798d853acb84178b4971723327c72de
Sanskar <92219526+Sanskar-08@users.noreply.github.com>
2022-10-08 20:46:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         """
     <style>
     [class="main css-k1vhr4 egzxvld3"] {
-        background-color: lightblue;
+        background-color: darkblue;
     }
     
     [class="css-1g1an1w edgvbvh9"]{
